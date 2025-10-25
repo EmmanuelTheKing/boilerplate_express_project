f207bf7e33a508f5873a2c805179faea0da3e688
Emmanuel Bila <126674304+EmmanuelTheKing@users.noreply.github.com>
2025-10-25 13:51:58 +0200

    Add files via upload

---
+++ myApp.js
@@ -0,0 +1,42 @@
+let express = require('express');
+let app = express();
+
+
+app.get('/',function(req,res){
+  res.send('Response String');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
