45c59dbed598b49d9f99d4637dc79e488dc21dc5
Emmanuel Bila <126674304+EmmanuelTheKing@users.noreply.github.com>
2025-10-25 14:25:44 +0200

    Enable static file serving for public directory
    Added static file serving for the '/public' directory.

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/',function(req,res){
   res.sendFile(__dirname + '/views/index.html');
@@ -40,3 +41,4 @@ app.get('/',function(req,res){
 
 
  module.exports = app;
+
